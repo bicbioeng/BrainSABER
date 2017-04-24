aa69b7d3ffcb89d6f19b7fec02e723f553992a7e
Carrie Jo Minette <carrie.minette@coyotes.usd.edu>
2017-04-24 18:03:37 -0500

    remove non-useful rows and columns from similarity matrices, document NanoSample1Set generation

--- R/getSimMatrix.R
+++ R/getSimMatrix.R
@@ -33,29 +33,36 @@ getSimMatrix <- function(relevantGenes, similarity_method = "cosine"){
   nrows <- nlevels(age)
   ncols <- nlevels(structure_acronym)
   #create and prefill the matrix, in case not all ages have all structures
-  mat <- matrix(data = rep(-1, nrows * ncols), nrow = nrows,
+  mat <- matrix(data = rep(NA, nrows * ncols), nrow = nrows,
                 ncol = ncols)
-  checkedMat <- matrix(data = rep(0, nrows * ncols), nrow = nrows,
-                       ncol = ncols)
   rownames(mat) <- as.character(levels(age))
   colnames(mat) <- as.character(levels(structure_acronym))
+  checkedMat <- mat
   #populate the matrix with similarity scores
   for(i in 1:length(age)){
     a <- as.character(age[i])
     s <- as.character(structure_acronym[i])
     mat[a,s] <- sim_score[i]
   }
+  # the following columns and rows are removed because they have 3 or less
+  # values in them
+  badcols <- c("CB", "CGE", "DTH", "LGE", "M1C-S1C", "MGE", "Ocx", "PCx", "TCx", "URL")
+  badrows <- c("25 pcw", "26 pcw","35 pcw")
+  inn <- which(colnames(mat) %in% badcols, arr.ind = TRUE)
+  inr <- which(rownames(mat) %in% badrows, arr.ind = TRUE)
+  mat <- mat[,-inn]
+  mat <- mat[-inr,]
+  # the following columns contain less than 5 different
+  #keepCols <- NULL
+  #for(i in 1:ncols){
+  #  if (sum(mat[, i]) > 1){
+  #    checkedMat[, i] <- mat[, i]
+  #    keepCols <- c(keepCols, colnames(mat)[i])
+  #  }
+  #}
 
-  keepCols <- NULL
-  for(i in 1:ncols){
-    if (sum(mat[, i]) > -10){
-      checkedMat[, i] <- mat[, i]
-      keepCols <- c(keepCols, colnames(mat)[i])
-    }
-  }
-
-  if (!(is.null(keepCols))){
-    mat <- checkedMat[, keepCols]
-  }
+ # if (!(is.null(keepCols))){
+  #  mat <- checkedMat[, keepCols]
+  #}
   return(mat)
 }
--- data-raw/NanoStringDataGeneration.R
+++ data-raw/NanoStringDataGeneration.R
@@ -3,6 +3,21 @@ library(nanoSet)
 data("NanoSet")
 # extract sample 1 as a named vector
 NanoSample1 <- exprs(NanoSet)[, 1]
-names(NanoSample1) <- fData(NanoSet)$RefSeq
+names(NanoSample1) <- as.character(fData(NanoSet)$gene_symbol)
 # get relevant subset of AIBSARNA
-NanoSampleSet <- getRelevantGenes(NanoSample1, gene_names = "RefSeq")
+NanoSampleSet <- getRelevantGenes(NanoSample1)
+#get indices of NanoSample1 that are present in NanoSampleSet
+genesToKeep <- which(names(NanoSample1) %in% fData(NanoSampleSet)$gene_symbol,
+              +               arr.ind = TRUE)
+#update NanoSample1 to only include comparable genes (genes in NanoSampleSet)
+NanoSample1 <- NanoSample1[genesToKeep]
+# remove any duplicate genes
+genesToKeep <- unique(names(NanoSample1))
+NanoSample1 <- NanoSample1[genesToKeep]
+
+NanoSampleSet <- getSimScores(NanoSample1, NanoSampleSet,
+                              similarity_method = "cosine")
+NanoSampleSet <- getSimScores(NanoSample1, NanoSampleSet,
+                              similarity_method = "euclidean")
+NanoSample1cosMat <- getSimMatrix(NanoSampleSet, similarity_method = "cosine")
+NanoSample1eucMat <- getSimMatrix(NanoSampleSet, similarity_method = "euclidean")
