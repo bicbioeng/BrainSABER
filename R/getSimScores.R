d42131d88ecb6cefeb5749012e401c377a787065
Carrie Jo Minette <carrie.minette@coyotes.usd.edu>
2017-05-15 17:01:46 -0500

    sim_matrix branch wasn't as done as I thought it was, now it's done

--- R/getSimScores.R
+++ R/getSimScores.R
@@ -1,41 +1,45 @@
 #' Get Similarity Scoring for a Gene Expression Vector
 #'
 #' This function computes the similarity score of a gene expression
-#' vector returned by \code{getExternalVector} or a trimmed data set returned by
-#' \code{getTrimmedExternalSet}, compared to a subset of AIBSARNA,
+#' vector returned by \code{getExternalVector} or a trimmed data set returned
+#' by \code{getTrimmedExternalSet}, compared to a subset of AIBSARNA,
 #' obtained by \code{getRelevantGenes}.
 #'
 #' @param data a named vector of gene expression values returned by
 #'     \code{getExternalVector}, or an ExpressionSet returned by
 #'     \code{getTrimmedExternalSet}
 #' @param relevantGenes an ExpressionSet returned by \code{getRelevantGenes}
-#' @param similarity_method currently supported similarity methods are "cosine"
-#'     and "euclidean", defaults to "cosine"
+#' @param similarity_method currently supported similarity methods are
+#'     "cosine" and "euclidean", defaults to "cosine"
 #'
-#' @return If \code{data} is a vector, returns a vector of similarity scores for
-#'     each sample in relevantGenes. If \code{data} is an ExpressionSet, returns
-#'     a data frame, with columns containing the similarity scores for and named
-#'     after each sample in \code{data}, and rows named after each sample in
-#'     \code{relevantGenes}
+#' @return If \code{data} is a vector, returns a vector of similarity scores
+#'     for each sample in relevantGenes. If \code{data} is an ExpressionSet,
+#'     returns a data frame, with columns containing the similarity scores
+#'     for and named after each sample in \code{data}, and rows named after
+#'     each sample in \code{relevantGenes}
 #' @import Biobase
 #' @export
 #' @examples
 #' myGenes <- c(4.484885, 0.121902, 0.510035)
 #' names(myGenes) <- c("TNFRSF1A", "BCL3", "NEFH")
 #' myGeneSet <- getRelevantGenes(myGenes, AIBSARNAid = "gene_symbol")
-#' CosScores <- getSimScores(myGenes, myGeneSet, similarity_method = "cosine")
-#' EucScores <- getSimScores(myGenes, myGeneSet, similarity_method = "euclidean")
+#' CosScores <- getSimScores(myGenes, myGeneSet,
+#'                           similarity_method = "cosine")
+#' EucScores <- getSimScores(myGenes, myGeneSet,
+#'                           similarity_method = "euclidean")
 #' myCosSimFrame <- getSimScores(myGeneSet, myGeneSet)
 
 getSimScores <- function(data, relevantGenes, similarity_method = "cosine"){
   if (is.vector(data)){
     if (similarity_method == "euclidean"){
-      euclidean_similarity <- apply(X = exprs(relevantGenes), MARGIN = 2,
-                                    FUN = euclideanSim, y = data)
+      euclidean_similarity <- as.vector(apply(X = exprs(relevantGenes),
+                                              MARGIN = 2,
+                                              FUN = euclideanSim, y = data))
       return(euclidean_similarity)
     } else {
-      cosine_similarity <- apply(X = exprs(relevantGenes), MARGIN = 2,
-                                 FUN = lsa::cosine, y = data)
+      cosine_similarity <- as.vector(apply(X = exprs(relevantGenes),
+                                           MARGIN = 2,
+                                           FUN = lsa::cosine, y = data))
       return(cosine_similarity)
     }
   } else {
