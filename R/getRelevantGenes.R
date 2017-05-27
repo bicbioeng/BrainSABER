d9506bad68b8715408366f38f5e1098851ecb6dd
Carrie Jo Minette <carrie.minette@coyotes.usd.edu>
2017-05-26 22:30:04 -0500

    getRelevantGenes now sorts the resulting ExpressionSet to match the order of input data

--- R/getRelevantGenes.R
+++ R/getRelevantGenes.R
@@ -1,21 +1,23 @@
 #' Get a subset of AIBSARNA using a Gene Expression Vector
 #'
 #' This function returns a subset of the AIBSARNA dataset, containing only
-#' the genes \code{data}, which may be a vector or a Biobase ExpressionSet, or a
-#' derivative of ExpressionSet that supports the Biobase methods \code{exprs()}
-#' and \code{fData()}.  If a vector is used, it must consist of numerical gene
-#' expression values with names comparable to one columns of identifiers present
-#' in AIBSARNA.
+#' the genes \code{data}, which may be a vector or a Biobase ExpressionSet,
+#' or a derivative of ExpressionSet that supports the Biobase methods
+#' \code{exprs()} and \code{fData()}.  If a vector is used, it must consist
+#' of numerical gene expression values with names comparable to one columns
+#' of identifiers present in AIBSARNA.
 #'
-#' @param data a vector of named gene expression values, or a compatible data set
-#' @param dataSetId (Optional) If \code{data} is not a vector, the name of the
-#'     column of gene identifiers in fData(dataSet) to be used to compare
+#' @param data a vector of named gene expression values, or a compatible
+#'     data set
+#' @param dataSetId (Optional) If \code{data} is not a vector, the name of
+#'     the column of gene identifiers in fData(dataSet) to be used to compare
 #'     \code{data} to AIBSARNA.
 #' @param AIBSARNAid the name of the column of fData(AIBSARNA) that is
 #'     comparable to dataSetId.  One of "gene_id", "ensembl_gene_id",
 #'     "gene_symbol", "entrez_id", "refseq_ids"
 #'
-#' @return a Biobase ExpressionSet consisting of genes in v
+#' @return a Biobase ExpressionSet consisting of genes in \code{data}, sorted
+#'     to match the order of the genes in \code{data}
 #' @import Biobase
 #' @import AIBSARNA
 #' @export
@@ -47,10 +49,26 @@ getRelevantGenes <- function(data, dataSetId = NULL,
   relfd <- fData(AIBSARNA::AIBSARNA)[vInd, ]
   # remove any unused factor levels
   relfd <- as.data.frame(apply(relfd, 2, function(x) {x[drop = TRUE]}))
-  # convert to Annotated Data Frame
-  relfd <- new("AnnotatedDataFrame", data = relfd)
   # subset exprs
   relexprs <- exprs(AIBSARNA::AIBSARNA)[vInd, ]
+  # sort relfd and relexprs so the genes are in the same order as dataSet
+  relfd[[AIBSARNAid]] <- factor(relfd[[AIBSARNAid]],
+                                levels = unique(names(v)))
+  relfd <- relfd[order(relfd[[AIBSARNAid]]),]
+  relfd$row_num <- factor(relfd$row_num, levels = unique(relfd$row_num))
+  # convert relexprs to data.frame and add a column to do factor
+  # manipulations
+  relexprs <- as.data.frame(relexprs)
+  row_num <- row.names(relexprs)
+  relexprs <- cbind(relexprs, row_num)
+  relexprs$row_num <- as.character(relexprs$row_num)
+  relexprs$row_num <- factor(relexprs$row_num,
+                             levels = trimws(as.character(relfd$row_num)))
+  relexprs <- relexprs[order(relexprs$row_num),]
+  # convert relexprs back to a matrix, remove the added column
+  relexprs <- as.matrix(relexprs[, -525])
+  # convert to Annotated Data Frame
+  relfd <- new("AnnotatedDataFrame", data = relfd)
   # put together ExpressionSet
   relevantGenes <- ExpressionSet(assayData = relexprs,
                                  phenoData = phenoData(AIBSARNA::AIBSARNA),
