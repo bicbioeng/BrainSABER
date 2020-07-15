b8dac7fd10ab0b765446b6a0bdf09406ccfb1954
Evgeni <evgeni.radichev@gmail.com>
2020-07-15 15:34:42 -0500

    changed gitignore dependencies

---
+++ R/runShinyBrainSABER.R
@@ -0,0 +1,23 @@
+#' Run the shiny application for the BrainSABER package workflow.
+#' 
+#' @return NULL (Invisibly)
+#' 
+#' @examples 
+#' if (interactive()) {
+#'     options(device.ask.default = FALSE)
+#'     runShinyBrainSABER()
+#' }
+
+runShinyBrainSaber <- function(){
+  # appDir <- system.file(file.path("inst","Shiny"), package = "BrainSABER")
+  # if (appDir == "") {
+  #   stop("Example directory not found. Try re-installing BrainSABER.",
+  #        call. = FALSE)
+  # }
+  # shiny::runApp(appDir, display.mode = "normal")
+  
+  ui = shinyAppUI
+  server= shinyAppServer
+  # Run the application
+  shiny::shinyApp(ui = ui, server = server)
+}
